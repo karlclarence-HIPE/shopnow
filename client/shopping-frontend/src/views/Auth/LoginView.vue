61571793b64923a33506c8893717b0ba2bdde471
karlclarence-HIPE <karl_clarence_rivera@hipe.asia>
2026-02-09 17:49:31 +0800

    feature change position within authentication pages

--- client/shopping-frontend/src/views/Auth/LoginView.vue
+++ client/shopping-frontend/src/views/Auth/LoginView.vue
@@ -24,14 +24,15 @@ const password = ref('');
                         placeholder=" " required />
                     <label for="email"
                         class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email
-                        address</label>
+                        address <span class="text-red-500">*</span></label>
                 </div>
                 <div class="relative z-0 w-1/2 mb-5 group">
                     <input type="password" name="password" id="password"
                         class="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
                         placeholder=" " required />
                     <label for="password"
-                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password</label>
+                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password
+                        <span class="text-red-500">*</span></label>
                 </div>
                 <div class="flex w-1/2 items-center gap-2 mb-5">
                     <input type="checkbox" id="remember" value=""
--- client/shopping-frontend/src/views/Auth/SignUpView.vue
+++ client/shopping-frontend/src/views/Auth/SignUpView.vue
@@ -7,24 +7,24 @@ import FooterView from '../../components/global/FooterView.vue';
 const name = ref('');
 const email = ref('');
 const password = ref('');
+const confirmPassword = ref('');
 </script>
 <template>
     <NavBar />
     <div class="min-h-screen flex items-center justify-center pt-10">
         <form action="" class="flex flex-row items-center justify-center w-full h-full">
-            <div class="hidden w-full md:flex flex-row items-center justify-center bg-gray-200">
-                <img :src="shop" alt="">
-            </div>
+
             <div class="w-[200vw] md:w-full flex flex-col items-center justify-center">
                 <div class="flex flex-col items-start justify-center z-0 mb-5 group w-1/2">
-                    <h2 class="text-2xl">Create an Account for ShopNow</h2>
+                    <h2 class="text-2xl">Create an Account to ShopNow</h2>
                 </div>
                 <div class="relative z-0 w-1/2 mb-5 group">
                     <input type="text" name="name" id="name"
                         class="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
                         placeholder=" " required />
                     <label for="name"
-                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Name</label>
+                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Name
+                        <span class="text-red-500">*</span></label>
                 </div>
                 <div class="relative z-0 w-1/2 mb-5 group">
                     <input type="email" name="email" id="email"
@@ -32,14 +32,15 @@ const password = ref('');
                         placeholder=" " required />
                     <label for="email"
                         class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Email
-                        address</label>
+                        address <span class="text-red-500">*</span></label>
                 </div>
                 <div class="relative z-0 w-1/2 mb-5 group">
                     <input type="password" name="password" id="password"
                         class="block py-2.5 px-0 w-full text-sm text-heading bg-transparent border-0 border-b-2 border-default-medium appearance-none focus:outline-none focus:ring-0 focus:border-brand peer"
                         placeholder=" " required />
                     <label for="password"
-                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password</label>
+                        class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Password
+                        <span class="text-red-500">*</span></label>
                 </div>
                 <div class="relative z-0 w-1/2 mb-5 group">
                     <input type="confirmPassword" name="confirmPassword" id="confirmPassword"
@@ -47,13 +48,7 @@ const password = ref('');
                         placeholder=" " required />
                     <label for="confirmPassword"
                         class="absolute text-sm text-body duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-left peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Confirm
-                        Password</label>
-                </div>
-                <div class="flex w-1/2 items-center gap-2 mb-5">
-                    <input type="checkbox" id="remember" value=""
-                        class="w-4 h-4 text-dark border border-default-medium rounded-xs bg-neutral-secondary "
-                        required />
-                    <label for="remember" class="flex items-center ">Remember Me</label>
+                        Password <span class="text-red-500">*</span></label>
                 </div>
                 <div class="w-full flex flex-row items-center justify-center mb-5">
                     <button type="submit"
@@ -66,6 +61,9 @@ const password = ref('');
                             class="font-medium  hover:underline text-blue-500">Login</a>
                     </p>
                 </div>
+            </div>
+            <div class="hidden w-full md:flex flex-row items-center justify-center bg-gray-200">
+                <img :src="shop" alt="">
             </div>
         </form>
     </div>
