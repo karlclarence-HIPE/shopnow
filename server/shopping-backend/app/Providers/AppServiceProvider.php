d616a30e3f87a20ea0796e59026e297282365aed
karlclarence-HIPE <karl_clarence_rivera@hipe.asia>
2026-02-19 09:19:54 +0800

    fix for port

--- server/shopping-backend/app/Providers/AppServiceProvider.php
+++ server/shopping-backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,24 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->overrideSanctumConfigurationToSupportRefreshToken();
+    }
+
+    private function overrideSanctumConfigurationToSupportRefreshToken()
+    {
+        Sanctum::$accessTokenAuthenticationCallback = function ($accessToken, $isValid) {
+            $abilities = collect($accessToken->abilities);
+
+            
+            return $isValid;
+        };
+
+        Sanctum::$accessTokenRetrievalCallback = function ($request) {
+            if (!$request->routeIs('refresh')) {
+                return str_replace('Bearer ', '', $request->headers->get('Authorization'));
+            }
+
+            return $request->cookie('refreshToken') ?? '';
+        }
     }
 }
